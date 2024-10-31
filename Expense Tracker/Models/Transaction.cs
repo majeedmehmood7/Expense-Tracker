c4314781a33a76cdbf74198f5b3ec2f153ad9e07
majeedmehmood7 <majeedmehmood7@gmail.com>
2024-10-31 12:10:28 +0500

    first commit

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TrabsactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } 
+
+        public int amount { get; set; }
+
+        [Column(TypeName = "navrchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
